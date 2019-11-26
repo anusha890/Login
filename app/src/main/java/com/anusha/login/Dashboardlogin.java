9df9ba5c801a4158e4684ad547253a27e740dad6
anusha890 <anushapokharel890@gmail.com>
2019-11-26 14:27:16 +0545

    Initial commit

---
+++ app/src/main/java/com/anusha/login/Dashboardlogin.java
@@ -0,0 +1,14 @@
+package com.anusha.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboardlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboardlogin);
+    }
+}
